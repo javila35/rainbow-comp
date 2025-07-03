7d8163a705c57883db259bfd7a92e546e8fd1566
Joe Avila <josepharthuravila@gmail.com>
2025-07-03 14:14:42 -0700

    rbac setup

---
+++ app/components/ProtectedRoute.tsx
@@ -0,0 +1,44 @@
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import { Role } from "../generated/prisma";
+import { hasRole } from "@/auth";
+
+interface ProtectedRouteProps {
+  children: React.ReactNode;
+  requiredRole?: Role;
+  fallback?: React.ReactNode;
+}
+
+export default function ProtectedRoute({
+  children,
+  requiredRole = "USER",
+  fallback = <div>Access denied</div>,
+}: ProtectedRouteProps) {
+    const { data:session, status } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (status === "loading") return;
+
+        if (!session) {
+            router.push("/");
+            return;
+        }
+
+        if (requiredRole && !hasRole(session.user.role, requiredRole)) {
+            router.push("/access-denied")
+            return;
+        }
+    }, [session, status, router, requiredRole]);
+
+    if (status === "loading") {
+        return <div>Loading...</div>;
+    }
+
+    if (!session || (requiredRole && !hasRole(session.user.role, requiredRole))) {
+        return fallback;
+    }
+
+    return <>{children}</>;
+}
